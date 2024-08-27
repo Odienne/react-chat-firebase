700ef923cfc9b5f3b27427ab5cd3a27357f0e4e4
Adam Odienne <odienne.adam@gmail.com>
2024-08-27 12:05:08 +0200

    mockup chat feature with css and emoji picker

--- components/chat/Chat.jsx
+++ components/chat/Chat.jsx
@@ -1,6 +1,21 @@
 import "./chat.css"
+import EmojiPicker from "emoji-picker-react";
+import {useState} from "react";
 
 const Chat = () => {
+    const [emojiPicking, setEmojiPicking] = useState(false);
+    const [text, setText] = useState("");
+    const [caretPosition, setCaretPosition] = useState(0);
+
+    const handleEmojiChoice = (e) => {
+        setText(prev => prev.slice(0, caretPosition) + e.emoji + prev.slice(caretPosition));
+        setEmojiPicking(false);
+    }
+
+    const handleInputChange = (e) => {
+        setCaretPosition(e.target.selectionStart)
+        setText(e.target.value);
+    }
     return (
         <div className="chat">
             <div className="chat__top">
@@ -17,16 +32,58 @@ const Chat = () => {
                     <img src="./info.png" alt="info"/>
                 </div>
             </div>
-            <div className="chat__center"></div>
+            <div className="chat__center">
+                <div className="chat__center__message">
+                    <img src="./avatar.png" alt="avatar"/>
+                    <div className="chat__center__message__text">
+                        <p>
+                            I forgot my charger at your place, can I come back rn?
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="chat__center__message chat__center__message__self">
+                    <div className="chat__center__message__text">
+                        <p>
+                            I forgot my charger at your place, can I come back rn?
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="chat__center__message">
+                    <img src="./avatar.png" alt="avatar"/>
+                    <div className="chat__center__message__text">
+                        <p>
+                            I forgot my charger at your place, can I come back rn?
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+                <div className="chat__center__message">
+                    <img src="./avatar.png" alt="avatar"/>
+                    <div className="chat__center__message__text">
+                        <img src="./placeholder-image.jpg" alt="image"/>
+                        <p>
+                            I forgot my charger at your place, can I come back rn?
+                        </p>
+                        <span>1 min ago.</span>
+                    </div>
+                </div>
+
+            </div>
             <div className="chat__bottom">
                 <div className="chat__bottom__icons">
                     <img src="./img.png" alt="img"/>
                     <img src="./camera.png" alt="camera"/>
                     <img src="./mic.png" alt="mic"/>
                 </div>
-                <input type="text" placeholder="Type a message..."/>
+                <input value={text} type="text" placeholder="Type a message..." onClick={handleInputChange}
+                       onChange={handleInputChange}/>
                 <div className="chat__bottom__emoji">
-                    <img src="./emoji.png" alt="emoji"/>
+                    <img onClick={() => setEmojiPicking(!emojiPicking)} src="./emoji.png" alt="emoji"/>
+                    <div className="chat__bottom__emoji__pickerContainer">
+                        <EmojiPicker open={emojiPicking} onEmojiClick={handleEmojiChoice}/>
+                    </div>
                 </div>
                 <button className="chat__bottom__send">Send</button>
             </div>
